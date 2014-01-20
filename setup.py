ffa7d42d7e65702e4c89bb5c3153e390cd1f407a
Tyler Kennedy <tk@tkte.ch>
2014-01-20 12:08:53 -0500

    Added a simple jawap script with a 'grep' command.

--- setup.py
+++ setup.py
@@ -3,20 +3,31 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="jawa",
+    name='jawa',
     packages=find_packages(),
-    version="1.0",
-    description="Doing fun stuff with JVM ClassFiles.",
-    author="Tyler Kennedy",
-    author_email="tk@tkte.ch",
-    url="http://github.com/TkTech/Jawa",
-    keywords=["java", "disassembly", "disassembler"],
+    version='1.0.0',
+    description='Doing fun stuff with JVM ClassFiles.',
+    author='Tyler Kennedy',
+    author_email='tk@tkte.ch',
+    url='http://github.com/TkTech/Jawa',
+    keywords=[
+        'java',
+        'disassembly',
+        'disassembler'
+    ],
     classifiers=[
-        "Programming Language :: Python",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Disassemblers"
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Disassemblers'
+    ],
+    scripts=[
+        'jawa/scripts/jawap'
+    ],
+    install_requires=[
+        'docopt',
+        'colorama'
     ]
 )
